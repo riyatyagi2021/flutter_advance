67b864e54318a937e17f45906a817d260215676a
riyatyagi2021 <riya.mobcoder@gmail.com>
2021-12-13 14:20:02 +0530

    all

--- lib/widgets/list_of_widgets.dart
+++ lib/widgets/list_of_widgets.dart
@@ -16,43 +16,45 @@ class _WidgetsListState extends State<WidgetsList> {
         child: Container(
           margin: EdgeInsets.all(30),
           child: Center(
-            child: Column(
-              children: [
-                /////constraints
-                Container(
-                    color: Colors.grey,
-                    constraints: BoxConstraints(
-                        maxHeight: 150,
-                        maxWidth: 300
-                    ),
-                    child: ShaderMask(
-                        shaderCallback: (rect){
-                          return RadialGradient(
-                              radius: 1,
-                              center: Alignment.topLeft,
-                              tileMode: TileMode.mirror,
-                              colors:[Colors.yellow, Colors.red]
-                          ).createShader(rect);
-                        }
-                          ,
-
-                        child: const Text("RIYA ",style: TextStyle(fontSize: 100),))),
-                SizedBox(height: 20,),
-                ElevatedButton(
-                  child: Text("Silver App Bar"),
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.green, padding: EdgeInsets.all(20)),
-                  onPressed: () => Navigator.push(
-                      context, MaterialPageRoute(builder: (context) => AbsorbPainter())),
-                ),
-                SizedBox(height: 20,),
-
-              ],
-            ),
+              child: Column(
+                  children: [
+                  /////constraints
+                  Container(
+                  color: Colors.grey,
+                  constraints: BoxConstraints(
+                      maxHeight: 150,
+                      maxWidth: 300
+                  ),
+                  child: ShaderMask(
+                    shaderCallback: (rect) {
+                      return RadialGradient(
+                          radius: 1,
+                          center: Alignment.topLeft,
+                          tileMode: TileMode.mirror,
+                          colors: [Colors.yellow, Colors.red]
+                      ).createShader(rect);
+                    },
+                    child: const Text("RIYA ",
+                    style: TextStyle(fontSize: 100),))),
+          SizedBox(height: 20,),
+          ElevatedButton(
+            child: Text("Silver App Bar"),
+            style: ElevatedButton.styleFrom(
+                primary: Colors.green, padding: EdgeInsets.all(20)),
+            onPressed: () =>
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AbsorbPainter())),
           ),
-        ),
+          SizedBox(height: 20,),
 
+          ],
+        ),
       ),
+    ),
+
+    )
+    ,
     );
   }
 }
